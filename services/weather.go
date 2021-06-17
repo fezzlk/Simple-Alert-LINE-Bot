e94106c23b196c58eea4db0f65727d0b6259daca
yusuketk <yusuketk@weseek.co.jp>
2021-06-18 03:40:30 +0900

    get data as json

--- services/weather.go
+++ services/weather.go
@@ -2,10 +2,10 @@ package main
 
 import (
 	"fmt"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 )
-
 
 func main() {
 	// リクエストの作成
@@ -24,5 +24,68 @@ func main() {
 	if err != nil{
 		panic(err)
 	}
-	fmt.Printf("%s", body)
+
+	// jsonを構造体へデコード
+	var data Response
+	if err := json.Unmarshal(body, &data); err != nil {
+		panic(err)
+	}
+	
+	fmt.Printf("%v",data)
+}
+
+type Response struct {
+	Cod     string `json:"cod"`
+	Message int    `json:"message"`
+	Cnt     int    `json:"cnt"`
+	List    []struct {
+		Dt   int `json:"dt"`
+		Main struct {
+			Temp      float64 `json:"temp"`
+			FeelsLike float64 `json:"feels_like"`
+			TempMin   float64 `json:"temp_min"`
+			TempMax   float64 `json:"temp_max"`
+			Pressure  int     `json:"pressure"`
+			SeaLevel  int     `json:"sea_level"`
+			GrndLevel int     `json:"grnd_level"`
+			Humidity  int     `json:"humidity"`
+			TempKf    float64 `json:"temp_kf"`
+		} `json:"main"`
+		Weather []struct {
+			ID          int    `json:"id"`
+			Main        string `json:"main"`
+			Description string `json:"description"`
+			Icon        string `json:"icon"`
+		} `json:"weather"`
+		Clouds struct {
+			All int `json:"all"`
+		} `json:"clouds"`
+		Wind struct {
+			Speed float64 `json:"speed"`
+			Deg   int     `json:"deg"`
+			Gust  float64 `json:"gust"`
+		} `json:"wind"`
+		Visibility int     `json:"visibility"`
+		Pop        float64 `json:"pop"`
+		Rain       struct {
+			ThreeH float64 `json:"3h"`
+		} `json:"rain,omitempty"`
+		Sys struct {
+			Pod string `json:"pod"`
+		} `json:"sys"`
+		DtTxt string `json:"dt_txt"`
+	} `json:"list"`
+	City struct {
+		ID    int    `json:"id"`
+		Name  string `json:"name"`
+		Coord struct {
+			Lat float64 `json:"lat"`
+			Lon float64 `json:"lon"`
+		} `json:"coord"`
+		Country    string `json:"country"`
+		Population int    `json:"population"`
+		Timezone   int    `json:"timezone"`
+		Sunrise    int    `json:"sunrise"`
+		Sunset     int    `json:"sunset"`
+	} `json:"city"`
 }
