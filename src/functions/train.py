3e6d1cc68e2cb90302be630d6714b4414fba425f
fezzlk <fezzlk796@gmail.com>
2021-08-10 17:30:10 +0900

    fix scraping

--- src/functions/train.py
+++ src/functions/train.py
@@ -9,18 +9,15 @@ def scrape_trains_delay_info(train_id):
     '''
 	<dd>タグにカッコまれた要素を取得(class="trouble"は何かしらの遅延が発生)
 	'''
-    pattern = re.compile(r'<dd class="trouble">([.\s\S]*?)</dd>')	# 正規オブジェクトの生成
+    pattern = re.compile(r'<dd class="trouble">([.\s\S]*?)</dd>') # 正規オブジェクトの生成
     result = pattern.findall(html)
-    print('result:::', result)
-    if result is None or len(result) == 0:
+    if len(result) == 0:
         return '遅延はありません。'
 	
     # 遅延情報を整形
     pattern = re.compile(r'<p>(.*?)<span>(.*?)</span></p>')
-    result = pattern.findall(result)
-    print(result)
-    return '遅延あり'
-    # return result[0][1] + result[0][2] # 遅延情報詳細 + 掲載日時
+    result = pattern.findall(result[0])
+    return result[0][0] + result[0][1] # 遅延情報詳細 + 掲載日時
 
 def get_trains_delay_info():
     '''
