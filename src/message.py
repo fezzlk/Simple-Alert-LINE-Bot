35dcf9cb695d20e6c240306369508cf197df1c88
fezzlk <fezzlk796@gmail.com>
2021-08-10 15:27:28 +0900

    format weather info for reply message

--- src/message.py
+++ src/message.py
@@ -1,11 +1,19 @@
 from linebot.models import TextSendMessage
+from .functions import weather
 
 
 def create_train_delay_message():
     return '電車遅延情報を取得'
 
 def create_weather_message():
-    return '天気情報を取得'
+    data = weather.get_weather()
+    res = data['city'] + 'の天気\n'
+    for date, info_of_the_day in data['data'].items():
+        res += '\n' + date
+        for time, info in info_of_the_day.items():
+            res += '\n' + ' '.join([time, str(info['temp']), info['weather']])
+        res += '\n'
+    return res
 
 def create_message(user_id, message):
     '''
