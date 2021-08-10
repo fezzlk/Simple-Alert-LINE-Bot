217eeab6d58434989259b979c3bd534d9b84fe1f
fezzlk <fezzlk796@gmail.com>
2021-08-10 11:00:48 +0900

    apply DEBUG

--- config.py
+++ config.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+DEBUG = os.getenv('DEBUG')
 LINEBOT_CHANNEL_SECRET = os.getenv('LINEBOT_CHANNEL_SECRET')
 LINEBOT_CHANNEL_ACCESS_TOKEN = os.getenv('LINEBOT_CHANNEL_ACCESS_TOKEN')
 OPEN_WEATHER_MAP_API_KEY = os.getenv('OPEN_WEATHER_MAP_API_KEY')
