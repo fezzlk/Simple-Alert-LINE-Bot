c1acad7826cf3d5575e1efe5d31afd3c120d27ee
fezzlk <fezzlk796@gmail.com>
2021-11-16 13:17:48 +0900

    create init file in service

---
+++ src/services/__init__.py
@@ -0,0 +1,7 @@
+from .RequestInfoService import RequestInfoService
+from .TrainService import TrainService
+from .WeatherService import WeatherService
+
+request_info_service = RequestInfoService()
+train_service = TrainService()
+weather_service = WeatherService()
