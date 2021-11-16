f930beda962ad8df70db483694b3183a64642867
fezzlk <fezzlk796@gmail.com>
2021-11-16 13:15:59 +0900

    setup connection to mongo

--- src/__init__.py
+++ src/__init__.py
@@ -1,15 +1,19 @@
 # flake8: noqa
 
 import config
-
-# setup for flask
 from flask import Flask
+from linebot import (LineBotApi, WebhookHandler)
+from .pymongo import mongo_client
 
+# setup flask app
 app = Flask(__name__)
 app.debug = bool(config.DEBUG)
 
+# setup connection to mongodb
+app.config["MONGO_URI"] = config.MONGO_URI
+mongo_client.init_app(app)
+
 # setup for line-bot-sdk
-from linebot import (LineBotApi, WebhookHandler)
 line = LineBotApi(config.LINEBOT_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(config.LINEBOT_CHANNEL_SECRET)
 
