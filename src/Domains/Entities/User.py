06f1680cf652f0c4198e510c2274380fdfeb90a8
fezzlk <fezzlk796@gmail.com>
2021-11-21 23:43:31 +0900

    fix attr of user

--- src/Domains/Entities/User.py
+++ src/Domains/Entities/User.py
@@ -14,14 +14,14 @@ class User:
     def __init__(
         self,
         _id: str = None,
-        name: str = None,
+        user_name: str = None,
         line_user_name: str = None,
         line_user_id: str = None,
         created_at: datetime = datetime.now(),
         updated_at: datetime = datetime.now(),
     ):
         self._id = _id
-        self.name = name
+        self.user_name = user_name
         self.line_user_name = line_user_name
         self.line_user_id = line_user_id
         self.created_at = created_at
