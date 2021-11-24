541f62d5978a30845cd099d62777d5df1cf3c43e
fezzlk <fezzlk796@gmail.com>
2021-11-24 21:54:21 +0900

    fix

--- src/UseCases/Stock/RegisterStockUseCase.py
+++ src/UseCases/Stock/RegisterStockUseCase.py
@@ -11,12 +11,12 @@ class RegisterStockUseCase(IUseCase):
     def execute(self) -> None:
         args = line_request_service.message.split()
 
-        keyword = args[0]
         goods_name = args[1] if len(args) >= 2 else None
         period = args[2] if len(args) >= 3 else None
         print(f'register stock req: {goods_name} {period}')
         new_stock = Stock(
             goods_name=goods_name,
+            owner_line_id=line_request_service.req_line_user_id,
         )
         stock_repository.create(new_stock)
         line_response_service.add_message('食材を登録しました')
