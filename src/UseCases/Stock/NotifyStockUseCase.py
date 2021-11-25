89868880f7c63d77fd8fd737657dfd5d6cb81458
fezzlk <fezzlk796@gmail.com>
2021-11-25 16:02:53 +0900

    create notif stock use case

---
+++ src/UseCases/Stock/NotifyStockUseCase.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+from src.UseCases.Interface.IUseCase import IUseCase
+from src.services import (
+    line_response_service,
+)
+from src.Infrastructure.Repositories import user_repository, stock_repository
+
+
+class NotifyStockUseCase(IUseCase):
+    def execute(self) -> None:
+        users = user_repository.find()
+        for user in users:
+            stocks = stock_repository.find(
+                {'owner_line_id': user.line_user_id})
+            messages = []
+            for stock in stocks:
+                elapsed_time = (datetime.now() - stock.created_at).days
+                messages.append(f'{stock.goods_name}: {elapsed_time}日経過')
+            line_response_service.add_message('\n'.join(messages))
+            line_response_service.push(to=user.line_user_id)
