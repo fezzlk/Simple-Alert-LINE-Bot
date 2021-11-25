ae8cbafe0340d815bc1034b86ac3c7d72b5d9d39
fezzlk <fezzlk796@gmail.com>
2021-11-25 10:44:40 +0900

    apply scss

--- src/__init__.py
+++ src/__init__.py
@@ -2,7 +2,9 @@
 
 from src import config
 from flask import Flask
+from flask_assets import Environment, Bundle
 from src.views import views_blueprint
+from src.handle_line_event import line_blueprint
 
 # setup flask app
 app = Flask(__name__)
@@ -10,3 +12,10 @@ app.debug = bool(config.DEBUG)
 
 # set endpoints for views
 app.register_blueprint(views_blueprint)
+app.register_blueprint(line_blueprint)
+
+# scss
+assets = Environment(app)
+assets.url = app.static_url_path
+scss = Bundle('scss/style.scss', filters='pyscss', output='all.css')
+assets.register('scss_all', scss)
