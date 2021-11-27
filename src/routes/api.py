aa23de6204c83145614c253c0d6ce83535addc36
fezzlk <fezzlk796@gmail.com>
2021-11-27 11:01:59 +0900

    add endpoint

--- src/routes/api.py
+++ src/routes/api.py
@@ -1,5 +1,6 @@
 from flask import Blueprint
 from src.UseCases.Stock.NotifyStockUseCase import NotifyStockUseCase
+from src.UseCases.Stock.CheckExpiredStockUseCase import CheckExpiredStockUseCase
 api_blueprint = Blueprint('api_blueprint', __name__, url_prefix='/_api/v1')
 
 '''
@@ -10,4 +11,10 @@ Endpoints for line push message
 @api_blueprint.route('/push_stock_info', methods=['post'])
 def push_stock_info():
     NotifyStockUseCase().execute()
+    return 'done'
+
+
+@api_blueprint.route('/check_expire', methods=['post'])
+def check_expire():
+    CheckExpiredStockUseCase().execute()
     return 'done'
