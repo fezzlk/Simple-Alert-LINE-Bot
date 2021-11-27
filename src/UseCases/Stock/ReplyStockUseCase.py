4747278dec8e593ba1e49a6a16fe6638f7c7b60e
fezzlk <fezzlk796@gmail.com>
2021-11-27 11:01:26 +0900

    change format of reply text stock list

--- src/UseCases/Stock/ReplyStockUseCase.py
+++ src/UseCases/Stock/ReplyStockUseCase.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from src.UseCases.Interface.IUseCase import IUseCase
 from src.services import (
     line_response_service,
@@ -8,6 +9,9 @@ from src.Infrastructure.Repositories import stock_repository
 class ReplyStockUseCase(IUseCase):
     def execute(self) -> None:
         stocks = stock_repository.find()
-        goods_name_list = [s.goods_name for s in stocks]
-        line_response_service.add_message('食材一覧を表示します。')
-        line_response_service.add_message('\n'.join(goods_name_list))
+        line_response_service.add_message('食材と登録日からの日数を表示します。')
+        messages = []
+        for stock in stocks:
+            elapsed_time = (datetime.now() - stock.created_at).days
+            messages.append(f'{stock.goods_name}: {elapsed_time}日')
+        line_response_service.add_message('\n'.join(messages))
