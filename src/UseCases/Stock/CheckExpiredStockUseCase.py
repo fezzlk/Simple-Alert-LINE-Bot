ea39b581b30172af60cbbc7302f2ebc4fe5549a5
fezzlk <fezzlk796@gmail.com>
2021-11-27 11:01:51 +0900

    create usecase to check expire stock

---
+++ src/UseCases/Stock/CheckExpiredStockUseCase.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from src.UseCases.Interface.IUseCase import IUseCase
+from src.services import (
+    line_response_service,
+)
+from src.Infrastructure.Repositories import user_repository, stock_repository
+
+
+class CheckExpiredStockUseCase(IUseCase):
+    def execute(self) -> None:
+        users = user_repository.find()
+        for user in users:
+            stocks = stock_repository.find(
+                {'owner_line_id': user.line_user_id}
+            )
+            messages = []
+            for stock in stocks:
+                if stock.expiry_date is None:
+                    continue
+                days_until_expire = (
+                    stock.expiry_date - datetime.now()
+                ).days
+                print(days_until_expire)
+                if days_until_expire < 0:
+                    messages.append(
+                        f'{stock.goods_name}: x ({days_until_expire * -1}日超過)')
+                elif days_until_expire == 0:
+                    messages.append(
+                        f'{stock.goods_name}: 今日まで')
+                elif days_until_expire < 7:
+                    messages.append(
+                        f'{stock.goods_name}: あと{days_until_expire}日')
+            if len(messages) == 0:
+                messages[0] = '賞味期限が近づいている食材はありません。'
+            line_response_service.add_message('\n'.join(messages))
+            line_response_service.push(to=user.line_user_id)
