ffcdbe9f0ace4d7813e86ea8d0826e8e5b78d80e
fezzlk <fezzlk796@gmail.com>
2021-12-22 22:20:23 +0900

    adjust date format

--- src/UseCases/Stock/ReplyStockUseCase.py
+++ src/UseCases/Stock/ReplyStockUseCase.py
@@ -10,11 +10,15 @@ from src.Infrastructure.Repositories import stock_repository
 class ReplyStockUseCase(IUseCase):
     def execute(self) -> None:
         stocks = stock_repository.find()
-        line_response_service.add_message('食材と登録日からの日数を表示します。')
         messages = []
         for stock in stocks:
-            elapsed_time = (datetime.now() - stock.created_at).days
-            messages.append(f'{stock.item_name}: {elapsed_time}日')
+            if stock.expiry_date is not None:
+                messages.append(
+                    f'{stock.item_name}: {stock.expiry_date.date()}')
+            else:
+                elapsed_time = (datetime.now() - stock.created_at).days
+                messages.append(f'{stock.item_name}: 登録から{elapsed_time}日')
+
         line_response_service.add_message('\n'.join(messages))
         line_response_service.add_message(
             f'webで確認する→ {config.SERVER_URL}/stock?openExternalBrowser=1')
