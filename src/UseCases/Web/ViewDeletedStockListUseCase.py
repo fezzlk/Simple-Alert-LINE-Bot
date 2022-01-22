85b19430e974fb920bf855d57a65df3304de956e
fezzlk <fezzlk796@gmail.com>
2022-01-22 14:47:28 +0900

    create use case for view deleted stock table

---
+++ src/UseCases/Web/ViewDeletedStockListUseCase.py
@@ -0,0 +1,32 @@
+from flask import (
+    session,
+    request,
+)
+from typing import Dict, Tuple
+from src.Domains.Entities.WebUser import WebUser
+from src.UseCases.Interface.IUseCase import IUseCase
+from src.Infrastructure.Repositories import (
+    stock_repository,
+)
+from src.models.StockViewModel import StockViewModel
+from src.routes.Forms.AddStockForm import AddStockForm
+
+
+class ViewDeletedStockListUseCase(IUseCase):
+    def execute(self) -> Tuple[Dict, AddStockForm]:
+        page_contents = dict(session)
+        page_contents['title'] = '削除済みストック一覧'
+
+        web_user: WebUser = page_contents['login_user']
+        stocks = stock_repository.find({
+            '$and': [
+                {'$or': [
+                    {'owner_id': web_user.linked_line_user_id},
+                    {'owner_id': web_user._id},
+                ]},
+                {'status': 2},
+            ],
+        })
+        page_contents['stocks'] = [StockViewModel(stock) for stock in stocks]
+        page_contents['labels'] = ['名前', '期限', '登録日']
+        return page_contents
--- src/routes/views.py
+++ src/routes/views.py
@@ -12,6 +12,7 @@ from src.UseCases.Web.ApproveLinkLineUserUseCase import ApproveLinkLineUserUseCa
 from src.UseCases.Web.DeleteStockUseCase import DeleteStockUseCase
 from src.UseCases.Web.RegisterWebUserUseCase import RegisterWebUserUseCase
 from src.UseCases.Web.ViewApproveLinkLineUseCase import ViewApproveLinkLineUseCase
+from src.UseCases.Web.ViewDeletedStockListUseCase import ViewDeletedStockListUseCase
 from src.UseCases.Web.ViewRegisterUseCase import ViewRegisterUseCase
 
 from src.UseCases.Web.ViewStockListUseCase import ViewStockListUseCase
@@ -102,20 +103,7 @@ def delete_stock():
 @ login_required
 @ set_message
 def view_deleted_stock_list():
-    page_contents = dict(session)
-    page_contents['title'] = '削除済みストック一覧'
-    web_user: WebUser = page_contents['login_user']
-    stocks = stock_repository.find({
-        '$and': [
-            {'$or': [
-                {'owner_id': web_user.linked_line_user_id},
-                {'owner_id': web_user._id},
-            ]},
-            {'status': 2},
-        ],
-    })
-    page_contents['stocks'] = [StockViewModel(stock) for stock in stocks]
-    page_contents['labels'] = ['名前', '期限', '登録日']
+    page_contents = ViewDeletedStockListUseCase().execute()
     return render_template(
         'pages/stock/trash.html',
         page_contents=page_contents,
