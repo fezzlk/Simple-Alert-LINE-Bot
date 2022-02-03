d9f64f53b132304d4d06add83699e156e420f516
fezzlk <fezzlk796@gmail.com>
2022-02-03 22:09:38 +0900

    update help

--- src/UseCases/Line/AddStockUseCase.py
+++ src/UseCases/Line/AddStockUseCase.py
@@ -18,7 +18,7 @@ class AddStockUseCase(IUseCase):
             line_response_service.add_message(
                 '"ストック登録 [アイテム名] [期限(任意)]" と送ってください。')
             line_response_service.add_message(
-                '期限のフォーマットは\nYYYY年MM月DD日: YYYYMMDD\n20YY年MM月DD日: YYMMDD\n今年MM月DD日: MMDD\n今月DD日: DD')
+                '[日付の入力方法]\nYYYY年MM月DD日: YYYYMMDD\n20YY年MM月DD日: YYMMDD\n今年MM月DD日: MMDD\n今月DD日: DD')
             return
 
         expiry_date = None
