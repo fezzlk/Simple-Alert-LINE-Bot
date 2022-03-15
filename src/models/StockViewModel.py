e7ba75a0eda9afa000509a95a4f773f3734c604d
fezzlk <fezzlk796@gmail.com>
2022-03-16 08:45:19 +0900

    overload constractor of Sto

--- src/models/StockViewModel.py
+++ src/models/StockViewModel.py
@@ -18,11 +18,21 @@ class StockViewModel:
 
     def __init__(
         self,
-        stock: Stock
+        _id: str = '',
+        item_name: str = '',
+        str_expiry_date: str = '',
+        str_created_at: str = '',
+        stock: Stock = None
     ):
-        self._id = '' if stock._id is None else stock._id
-        self.item_name = '' if stock.item_name is None else stock.item_name
-        self.str_expiry_date = '' if stock.expiry_date is None else stock.expiry_date.date(
-        ).strftime('%Y/%m/%d')
-        self.str_created_at = '' if stock.created_at is None else stock.created_at.date(
-        ).strftime('%Y/%m/%d')
+        if stock is not None:
+            self._id = '' if stock._id is None else stock._id
+            self.item_name = '' if stock.item_name is None else stock.item_name
+            self.str_expiry_date = '' if stock.expiry_date is None else stock.expiry_date.date(
+            ).strftime('%Y/%m/%d')
+            self.str_created_at = '' if stock.created_at is None else stock.created_at.date(
+            ).strftime('%Y/%m/%d')
+        else:
+            self._id = _id
+            self.item_name = item_name
+            self.str_expiry_date = str_expiry_date
+            self.str_created_at = str_created_at
