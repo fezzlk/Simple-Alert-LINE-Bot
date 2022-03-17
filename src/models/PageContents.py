c066ce1a35150d9efcaa0c55e1e5455fabec4fd1
fezzlk <fezzlk796@gmail.com>
2022-03-17 21:49:32 +0900

    create RegisterFormData class for PageContents data

--- src/models/PageContents.py
+++ src/models/PageContents.py
@@ -8,6 +8,18 @@ from src.models.StockViewModel import StockViewModel
 
 
 @dataclass()
+class RegisterFormData:
+    login_name: str
+    login_email: str
+
+    def __init__(
+        self,
+        session: SessionMixin,
+    ):
+        self.login_name = session['login_name']
+        self.login_email = session['login_email']
+
+@dataclass()
 class StockListData:
     stocks: List[StockViewModel]
     keys: List[str]
@@ -15,12 +27,13 @@ class StockListData:
 
     def __init__(
         self,
+        session: SessionMixin,
     ):
         self.stocks = []
         self.keys = []
         self.labels = []
 
-T = TypeVar('T', StockListData, None)
+T = TypeVar('T', RegisterFormData, StockListData, None)
 
 @dataclass()
 class PageContents(Generic[T]):
@@ -29,7 +42,6 @@ class PageContents(Generic[T]):
     page_title: str
     login_user: WebUser
     line_user_name: str
-    login_email: str
     next_page_url: str
     data: T
     message: str
@@ -43,11 +55,10 @@ class PageContents(Generic[T]):
     ):
         self.session = dict(session)
         self.login_user = session.get('login_user', None)
-        self.login_email = session.get('login_email', None)
         self.next_page_url = session.get('next_page_url', '')
         self.line_user_name = ''
         self.page_title = page_title
         self.request = request
         self.message = ''
         if DataClass is not None:
-            self.data = DataClass()
+            self.data = DataClass(session)
