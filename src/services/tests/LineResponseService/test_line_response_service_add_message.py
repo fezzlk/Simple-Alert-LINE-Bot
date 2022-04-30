f7b52e44aea8b44cc6498dbedf2d463aee750036
fezzlk <fezzlk796@gmail.com>
2022-04-30 00:23:37 +0000

    ut of LineResponseService add_message

---
+++ src/services/tests/LineResponseService/test_line_response_service_add_message.py
@@ -0,0 +1,34 @@
+from src.services.LineResponseService import LineResponseService
+from src.tests.dummies import (
+    generate_dummy_follow_event,
+)
+from src.line_bot_api import line_bot_api
+from src.tests.dummies import Profile
+
+
+def test_success():
+    # Arrange
+    line_request_service = LineResponseService()
+
+    # Act
+    line_request_service.add_message('test')
+
+    # Assert
+    assert len(line_request_service.texts) == 1
+    assert line_request_service.texts[0].text == 'test'
+
+
+def test_success_multi_call():
+    # Arrange
+    line_request_service = LineResponseService()
+
+    # Act
+    line_request_service.add_message('test1')
+    line_request_service.add_message('test2')
+    line_request_service.add_message('test3')
+
+    # Assert
+    assert len(line_request_service.texts) == 3
+    assert line_request_service.texts[0].text == 'test1'
+    assert line_request_service.texts[1].text == 'test2'
+    assert line_request_service.texts[2].text == 'test3'
