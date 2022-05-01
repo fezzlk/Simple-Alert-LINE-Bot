e174aa33d6358d9fcd178816c9651d2e5205b59b
fezzlk <fezzlk796@gmail.com>
2022-05-01 23:33:54 +0000

    refactor line model

---
+++ src/models/Line/Event.py
@@ -0,0 +1,54 @@
+from dataclasses import dataclass
+from .Source import Source
+from .Message import Message
+from .Postback import Postback
+
+
+@dataclass()
+class Event:
+    event_type: str
+    event_list = ['follow', 'unfollow', 'message']  # TODO 要調査
+
+    type: str  # source type
+    user_id: str
+    group_id: str
+    message_type: str
+    text: str
+    postback_data: str
+    mode: str
+    reply_token: str
+    postback: Postback
+    source: Source
+
+    def __init__(
+        self,
+        event_type='message',
+        source_type='user',
+        user_id='',
+        group_id='',
+        message_type='text',
+        text='dummy_text',
+        postback_data='dummy_postback_data',
+        mode='active',
+    ):
+        if event_type in self.event_list:
+            self.event_type = event_type
+        else:
+            raise ValueError(
+                f'{event_type} is invalid as "event_type" of Event')
+
+        self.source = Source(
+            user_id=user_id,
+            source_type=source_type,
+            group_id=group_id)
+
+        self.mode = mode
+
+        if self.type == 'follow':
+            self.reply_token = 'dummy_reply_token'
+        if self.type == 'message':
+            self.reply_token = 'dummy_reply_token'
+            self.message = Message(text=text, message_type=message_type)
+        if self.type == 'postback':
+            self.reply_token = 'dummy_reply_token'
+            self.postback == Postback(data=postback_data)
