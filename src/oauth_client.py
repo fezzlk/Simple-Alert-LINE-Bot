98bc8be909e9ac7e3e123e7ff2ee9a8676084b67
fezzlk <fezzlk796@gmail.com>
2026-02-10 08:07:19 +0900

    Add LINE OpenID metadata URL for jwks

--- src/oauth_client.py
+++ src/oauth_client.py
@@ -7,6 +7,7 @@ oauth.register(
     name='line',
     client_id=config.LINE_LOGIN_CHANNEL_ID,
     client_secret=config.LINE_LOGIN_CHANNEL_SECRET,
+    server_metadata_url='https://access.line.me/.well-known/openid-configuration',
     access_token_url='https://api.line.me/oauth2/v2.1/token',
     authorize_url='https://access.line.me/oauth2/v2.1/authorize',
     api_base_url='https://api.line.me/',
