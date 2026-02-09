3091aec22bdf43f20aa58645fccf83b2b3754b64
fezzlk <fezzlk796@gmail.com>
2026-02-10 08:19:53 +0900

    Use LINE Login scope profile only to avoid id_token verification

--- src/oauth_client.py
+++ src/oauth_client.py
@@ -11,5 +11,5 @@ oauth.register(
     access_token_url='https://api.line.me/oauth2/v2.1/token',
     authorize_url='https://access.line.me/oauth2/v2.1/authorize',
     api_base_url='https://api.line.me/',
-    client_kwargs={'scope': 'profile openid'},
+    client_kwargs={'scope': 'profile'},
 )
